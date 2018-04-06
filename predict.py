8b6d87a8f760d705c1837da4c7f97426e7130e6a
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-04-06 23:01:20 +0700

    redeploy

--- predict.py
+++ predict.py
@@ -7,7 +7,7 @@ import h5py
 
 def predict_class(img_input):
     model = MobileNet(include_top=True, weights=None, classes=2, pooling='max', input_shape=(200, 200, 3))
-    model = model.load_weights(APP_ROOT + "/static/1_MobileNet_wood_weight.hdf5")
+    model.load_weights(APP_ROOT + "/static/1_MobileNet_wood_weight.hdf5")
     print("path: " + img_input)
     print(np.shape(np.expand_dims(image_operation.image_input(APP_ROOT + "/" + img_input),axis=0)))
     img = np.expand_dims(image_operation.image_input(APP_ROOT + "/" + img_input),axis=0)
