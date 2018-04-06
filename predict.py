4e90ed19cb820efbb23bc9bae0cced491856a7b7
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-04-06 23:56:53 +0700

    fixing

--- predict.py
+++ predict.py
@@ -1,15 +1,17 @@
 from keras.applications import MobileNet
-from app import APP_ROOT
 import keras
 import image_operation
 import numpy as np
 import h5py
+import os
+
+APP_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 def predict_class(img_input):
     model = MobileNet(include_top=True, weights=None, classes=2,
                       pooling='max', input_shape=(200, 200, 3))
-    model.load_weights(APP_ROOT + "/static/1_MobileNet_wood_weight.hdf5")
+    model.load_weights("./static/1_MobileNet_wood_weight.hdf5")
     print("path: " + img_input)
     print(np.shape(np.expand_dims(image_operation.image_input(
         APP_ROOT + "/" + img_input), axis=0)))
